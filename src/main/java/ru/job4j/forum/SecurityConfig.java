d76b2e2bd0986008a4e66b08293c7a6363d14033
meavv <c.c.c.p@bk.ru>
2022-06-13 15:29:05 +0500

    2. Spring boot security [#296071]

--- src/main/java/ru/job4j/forum/SecurityConfig.java
+++ src/main/java/ru/job4j/forum/SecurityConfig.java
@@ -10,19 +10,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    private DataSource ds;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-                .withUser("1").password(passwordEncoder.encode("1")).roles("ADMIN");
+        auth.jdbcAuthentication().dataSource(ds)
+                .usersByUsernameQuery("select username, password, enabled "
+                        + "from users "
+                        + "where username = ?")
+                .authoritiesByUsernameQuery(
+                        " select u.username, a.authority "
+                                + "from authorities as a, users as u "
+                                + "where u.username = ? and u.authority_id = a.id");
     }
 
     @Bean
--- src/main/java/ru/job4j/forum/model/User.java
+++ src/main/java/ru/job4j/forum/model/User.java
@@ -1,12 +1,22 @@
 package ru.job4j.forum.model;
 
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
+    private String username;
     private String password;
+    private boolean enabled;
+
+    @ManyToOne
+    @JoinColumn(name = "authority_id")
+    private Authority authority;
 
     public int getId() {
         return id;
@@ -17,11 +27,27 @@ public class User {
     }
 
     public String getName() {
-        return name;
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean active) {
+        this.enabled = active;
+    }
+
+    public Authority getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(Authority authority) {
+        this.authority = authority;
+    }
+
+    public void setName(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
